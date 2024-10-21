92f8d96e3cf26c26379eea19e76b327f5d654c9c
Aayush <aayush.077bct06@acem.edu.np>
2024-10-21 22:13:03 +0545

    code added for event,time,tithi

--- nepalicalendar.py
+++ nepalicalendar.py
@@ -1,8 +1,10 @@
 ''' module that print the Nepali Calendar '''
 
-from monthdays import check_monthDays
+from info import check_monthDays,IMPORTANT_EVENTS,tithi_names
 import calendar
-from datetime import date
+from datetime import date,datetime
+import requests
+import ephem
 
 def BS(englishYear,englishMonth,englishDate):
     refEnglishYear=1944
@@ -38,7 +40,18 @@ def BS(englishYear,englishMonth,englishDate):
         if(daycount>6):
             daycount=0
         diff-=1
-    return (f"{NepaliYear}-{NepaliMonth}-{NepaliDate} ({calendar.day_name[daycount]})")
+    return ([f"{NepaliYear}-{NepaliMonth}-{NepaliDate}",f"{calendar.day_name[daycount]}"])
+
+def event(year,month,date):
+    a=BS(year,month,date)[0]
+    a=a.split("-")
+    month=int(a[1])
+    date=int(a[2])
+    a=(month,date)
+    if(a in IMPORTANT_EVENTS):
+        return(IMPORTANT_EVENTS[a])
+    else:
+        return("No event")
 
 
 
@@ -67,9 +80,9 @@ def nepcalendar(year,month):
         adyear=year-57
     addate=27
     a=BS(adyear,admonth,addate)
-    a=a.replace(" ","-").replace("(","").replace(")","").split("-")
-    caldate=a[2]
-    calday=a[3][:3]
+    b=a[0].split("-")
+    caldate=b[2]
+    calday=a[1][:3]
     index=day.index(calday)
     x=int(caldate)
     while(x!=1):
@@ -99,3 +112,64 @@ def nepcalendar(year,month):
 
     mainlist.extend(bodylist)
     return(" ".join(mainlist))
+
+
+def time():
+    now = datetime.now()
+    time_12hr = now.strftime("%I:%M %p")
+    return time_12hr
+
+def get_current_location():
+    try:
+        response = requests.get('https://ipinfo.io/json')
+        data = response.json()
+        location = data['loc'].split(',')
+        latitude, longitude = location[0], location[1]
+        return latitude, longitude
+    except Exception as e:
+        raise ValueError("Could not determine location: " + str(e))
+    
+def tithi(date_in):
+    latitude, longitude = get_current_location()
+
+    
+    observer = ephem.Observer()
+    observer.lat = latitude  
+    observer.lon = longitude  
+    
+    date_in = ephem.Date(date_in)  
+    observer.date = date_in
+
+    # Get solar longitude
+    sun = ephem.Sun(observer)
+    solar_longitude = sun.hlon  
+
+    # Get lunar longitude
+    moon = ephem.Moon(observer)
+    lunar_longitude = moon.hlon  
+
+    
+    new_moon = ephem.previous_new_moon(observer.date)
+
+    
+    lunar_age = observer.date - new_moon  
+
+    
+    tithi = int(lunar_age * 30 / 29.53) + 1  
+
+    
+    paksha = "Shukla Paksha" if tithi <= 15 else "Krishna Paksha"
+
+    return f"{tithi_names[tithi]},{paksha}"
+
+def nepali(year,month,dates):
+    result={}
+    bs=BS(year,month,dates)
+    result['date']=bs[0]
+    result['day']=bs[1]
+    result['time']=time()
+    result['tithi']=tithi(date(year,month,dates))
+    e=event(year,month,dates)
+    result['event']=e
+    return(result)
+
